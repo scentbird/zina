e0561096db25bef9d601681d2d9b7ab287fd4909
Pavel Ivanov <grammka@gmail.com>
2019-08-29 16:54:13 +0300

    v1.1.1

--- babel.config.js
+++ babel.config.js
@@ -2,18 +2,8 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env', {
-        targets: {
-          browsers: [
-            'last 2 versions',
-          ],
-        },
         modules: false,
       },
     ],
-  ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-spread',
   ],
 }
