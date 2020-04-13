34be44f686245d9ebcd6bba66950de5c4baabd8f
Pavel Ivanov <grammka@gmail.com>
2020-04-13 14:09:47 +0300

    update dependencies, add IE >= 11 to preset-env browsers

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,20 @@
-module.exports = (api) =>
-  api.env('test')
-    ? {
-      presets: [
-        '@babel/env',
-        '@babel/typescript',
-      ],
-    }
-    : {
-      presets: [
-        [ '@babel/env', { targets: { node: 'current' } } ],
-        '@babel/typescript',
-      ],
-    }
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env', {
+      targets: {
+        browsers: [
+          'last 2 major versions',
+          'not dead',
+          'safari >= 9',
+          'iOS >= 9',
+          'ie >= 11',
+        ],
+      },
+      useBuiltIns: 'usage',
+      corejs: '3',
+    },
+    ],
+    '@babel/preset-typescript',
+  ],
+}
